e9e58baeaaac8da53da89ac5319a28f2bf616c4c
Laura Alvear <lauvickysofi@gmail.com>
2023-08-06 20:22:38 -0300

    Trabajando con números

---
+++ python-basics/numbers.py
@@ -0,0 +1,44 @@
+import math
+
+print(bin(55))
+
+print(hex(33))
+
+# Operadores Aritméticos
+
+x = 10 + 3
+x = 10 - 3
+x = 10 * 3
+x = 10 / 3
+x = 10 // 3
+x = 10 % 3
+x = 10 ** 3
+x = x +1
+x += 1 #incrementa x en 1
+
+print(x)
+
+#Working with numbers
+
+PI = -3.14
+print(round(PI))
+print(abs(PI))
+
+print(math.floor(PI))
+
+# Type conversion
+
+x = input("x: ")
+
+print(int(x))
+print(float(x))
+print(bool(x))
+
+#falsy
+#""
+#0
+#()
+#None (null)
+
+
+
