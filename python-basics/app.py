e09717003191e61089589ee73e8100f72b5b2bc6
Laura Alvear <lauvickysofi@gmail.com>
2023-08-06 17:35:27 -0300

    Iniciando el curso

---
+++ python-basics/app.py
@@ -0,0 +1,32 @@
+students_count = 1000
+
+rating = 4.99
+
+is_published = False
+
+curse_name = """Multiple 
+lines"""
+
+x, y = 1, 2
+
+a = b = 1
+
+print(type(students_count))
+
+age: int = 20
+age = "Python"
+print(age)
+
+x = 1
+print(id(x))
+
+x = x +1
+print(id(x))
+
+x = [1, 2, 3]
+print(id(x))
+
+x.append(4)
+print(id(x))
+
+
