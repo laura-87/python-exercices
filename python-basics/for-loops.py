951964641b48d24b5ad8516c75f57b13d6ecbdad
Laura Alvear <lauvickysofi@gmail.com>
2023-08-07 09:45:45 -0300

    for loops

---
+++ python-basics/for-loops.py
@@ -0,0 +1,15 @@
+
+#for x in "Python":
+#   print(x)
+
+#for x in ['a', 'b', 'c']:
+#   print(x)
+
+#for x in range(5):
+#   print(x)
+
+#for x in range(0, 10, 2):
+#    print(x)
+
+print(type(range(50000000)))
+print([1, 2, 3, 4, 5])
