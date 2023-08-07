b1162009fd398251d55776aded7720ee0cb68a59
Laura Alvear <lauvickysofi@gmail.com>
2023-08-07 11:52:15 -0300

    Trabajamos con arguments(x,xx)

--- python-basics/functions.py
+++ python-basics/functions.py
@@ -3,3 +3,25 @@ def increment(number: int, by: int = 1) -> tuple[int, int]:
 
 
 print(increment(2))
+
+
+# Arguments xargs
+
+def multiply(*list):
+    total = 1
+    for number in list:
+        total *= number
+    return total
+
+
+print(multiply(2, 3, 4, 5))
+
+
+# Arguments xxargs
+
+def save_user(**user):
+    print(user["id"])
+    print(user["name"])
+
+
+save_user(id=1, name="admin")
