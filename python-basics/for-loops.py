f763aec6af023ca997342e6d7d498b3909daafaa
Laura Alvear <lauvickysofi@gmail.com>
2023-08-07 10:11:05 -0300

    Trabajamos con FOR ELSE

--- python-basics/for-loops.py
+++ python-basics/for-loops.py
@@ -11,5 +11,24 @@
 #for x in range(0, 10, 2):
 #    print(x)
 
-print(type(range(50000000)))
-print([1, 2, 3, 4, 5])
+#print(type(range(50000000)))
+#print([1, 2, 3, 4, 5])
+
+"""names = ["AJohn", "Mary"]
+found = False
+for name in names:
+    if name.startswith("J"):
+        print("Found")
+        found = True
+        break
+
+if not found:
+    print("Not found")"""
+
+names = ["AJohn", "Mary"]
+for name in names:
+    if name.startswith("J"):
+        print("Found")
+        break
+else:
+    print("Not found")
